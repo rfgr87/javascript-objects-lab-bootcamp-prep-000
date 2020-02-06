5f98c7f175cf42cf0076ecc9d77244d89e8bf5ee
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-06 00:56:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var object2 = Object.assing 
+  var object2 = Object.assign({}, object) 
   object[key] = value
   object2 = object
   return object2
